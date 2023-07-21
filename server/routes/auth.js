073e90022823d01b737f012ea1b0b69e824e6128
MinhThong14 <achilles.mai14@gmail.com>
2023-07-21 16:28:25 -0400

    chore: add login controller and router

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 const {
-    register,
+    register, login,
 } = require("../controllers/userController");
 
 const router = require("express").Router();
 
 router.post("/register", register);
+router.post("/login", login)
 
 module.exports = router;

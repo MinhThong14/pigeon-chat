e8a4156c42ffce12aba2e38b53fcc7bed09600fc
MinhThong14 <achilles.mai14@gmail.com>
2023-07-20 23:46:25 -0400

    chore: add unit test for register api

--- server/index.js
+++ server/index.js
@@ -1,28 +1,20 @@
 const mongoose = require("mongoose");
 const express = require("express")
-const cors = require("cors");
-const authRoutes = require("./routes/auth")
 
-const app = express();
+const app = require("./app");
+
 require("dotenv").config();
-
-app.use(cors());
-app.use(express.json());
-
-
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("DB Connection Successful");
-    })
-    .catch((err) => {
-        console.log(err.message);
-    });
-
-app.use("/api/auth", authRoutes)    
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("DB Connection Successful");
+})
+.catch((err) => {
+    console.log(err.message);
+});
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server Started on Port ${process.env.PORT}`);

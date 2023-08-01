d1c749d9c44fd54144fff5a3adb11a9eb78019fb
MinhThong14 <achilles.mai14@gmail.com>
2023-07-31 21:14:54 -0400

    chore: add avatar

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,11 @@
 const {
-    register, login,
+    register, login, setAvatar
 } = require("../controllers/userController");
 
 const router = require("express").Router();
 
 router.post("/register", register);
 router.post("/login", login)
+router.post("/setavatar/:id", setAvatar);
 
 module.exports = router;
